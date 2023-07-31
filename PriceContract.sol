a70d3581426a9883ec6c32a56fe047e1e26ad587
mojowlekaz <93149541+mojowlekaz@users.noreply.github.com>
2023-07-31 13:11:28 +0100

    new

---
+++ PriceContract.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+interface ISwitchboard {
+    // read from aggregator
+    function latestResult(
+        address aggregatorAddress
+    ) external payable returns (int256 value, uint timestamp);
+}
+
+contract PriceContract {
+    int256 public latestValue;
+    uint256 public latestTimestamp;
+    address switchboardAddress;
+    address aggregatorAddress;
+
+    constructor(address _switchboard, address _aggregatorAddress) {
+        switchboardAddress = _switchboard;
+        aggregatorAddress = _aggregatorAddress;
+    }
+
+    function latest() external view returns (int256, uint256) {
+        return (latestValue, latestTimestamp);
+    }
+
+    function getPrice() external returns (uint256) {
+        ISwitchboard switchboard = ISwitchboard(switchboardAddress);
+        (int256 value, ) = switchboard.latestResult(aggregatorAddress);
+        latestValue = value;
+        latestTimestamp = block.timestamp;
+        return uint256(value);
+    }
+}
